7071fcd13203604c5761ef773f1c538d3f21b364
NPotokin <npotokin@gmail.com>
2023-01-12 13:31:36 -0700

    added js file, started assigning variables

---
+++ game.js
@@ -0,0 +1,18 @@
+
+    //Defining Variables
+    const rock = document.getElementById("rock")
+    const paper = document.getElementById("paper")
+    const scissors = document.getElementById("scissors")
+    const lizard = document.getElementById("lizard")
+    const spock = document.getElementById("spock")
+    
+    const computerSelection = document.getElementById("computerSelection")
+    const userSelection = document.getElementById("userSelection");
+
+    // rock.onclick = function() {
+    //     alert("NOOOOO");
+    // } 
+
+
+
+
